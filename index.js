ef10aea1a2bbbd69fc69d0c0becbf6605c776ea1
Bottomloader <imsamlolll@gmail.com>
2025-04-06 18:44:29 +0300

    Function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const toWords = require("./src/functions/toWords.js");
 const toNumbers = require("./src/functions/toNumbers.js");
-const formatNumbers = require("./src/functions/formatNumbers.js");
+const formatNumber = require("./src/functions/formatNumber.js");
 const toOrdinal = require("./src/functions/toOrdinal.js");
 const toWordsDate = require("./src/functions/toWordsDate.js");
 const ntoRoman = require("./src/functions/ntoRoman.js");
@@ -8,7 +8,7 @@ const ntoRoman = require("./src/functions/ntoRoman.js");
 module.exports = {
   toWords,
   toNumbers,
-  formatNumbers,
+  formatNumber,
   toOrdinal,
   toWordsDate,
   ntoRoman
